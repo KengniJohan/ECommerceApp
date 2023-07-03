d30d2a293bb44945523342896e50b182a4fe3490
KengniJohan <107089322+KengniJohan@users.noreply.github.com>
2023-07-03 15:06:05 +0100

    Création du binding du dashboard

---
+++ e_commerce_app/lib/view/dashboard/dashboard_binding.dart
@@ -0,0 +1,10 @@
+import 'package:e_commerce_app/controller/dashboard_controller.dart';
+import 'package:get/get.dart';
+
+class DashboardBinding extends Bindings {
+  @override
+  void dependencies() {
+    Get.put(DashboardController());
+  }
+
+}
