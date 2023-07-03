35193ce90064518a92b408a61d05fb0e205567aa
KengniJohan <107089322+KengniJohan@users.noreply.github.com>
2023-07-03 16:18:21 +0100

    Utilisation de l'écran principal

--- e_commerce_app/lib/view/dashboard/dashboard_screen.dart
+++ e_commerce_app/lib/view/dashboard/dashboard_screen.dart
@@ -1,4 +1,5 @@
 import 'package:e_commerce_app/controller/dashboard_controller.dart';
+import 'package:e_commerce_app/view/home/home_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_snake_navigationbar/flutter_snake_navigationbar.dart';
 import 'package:get/get.dart';
@@ -14,7 +15,7 @@ class DashboardScreen extends StatelessWidget {
           child: IndexedStack(
             index: controller.tabIndex,
             children: [
-              Container(),
+              const HomeScreen(),
               Container(
                 color: Colors.red,
               ),
