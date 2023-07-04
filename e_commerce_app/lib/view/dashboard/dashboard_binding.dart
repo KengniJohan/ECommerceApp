3f347df8c9e136aacae750106136049c7d26b232
KengniJohan <107089322+KengniJohan@users.noreply.github.com>
2023-07-04 10:59:07 +0100

    Consommation du HomeController

--- e_commerce_app/lib/view/dashboard/dashboard_binding.dart
+++ e_commerce_app/lib/view/dashboard/dashboard_binding.dart
@@ -1,10 +1,12 @@
 import 'package:e_commerce_app/controller/dashboard_controller.dart';
+import 'package:e_commerce_app/controller/home_controller.dart';
 import 'package:get/get.dart';
 
 class DashboardBinding extends Bindings {
   @override
   void dependencies() {
     Get.put(DashboardController());
+    Get.put(HomeController());
   }
 
 }
