60f99d60b3c018b67fbbb43b75fa49043b808f77
KengniJohan <107089322+KengniJohan@users.noreply.github.com>
2023-07-04 13:34:45 +0100

    Consommation de la CarouselSliderView

--- e_commerce_app/lib/view/home/home_screen.dart
+++ e_commerce_app/lib/view/home/home_screen.dart
@@ -1,5 +1,6 @@
 import 'package:e_commerce_app/component/main_header.dart';
 import 'package:e_commerce_app/controller/controllers.dart';
+import 'package:e_commerce_app/view/home/components/carousel_slider/carousel_slider_view.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
@@ -15,8 +16,8 @@ class HomeScreen extends StatelessWidget {
         children: [
           const MainHeader(),
           Obx(() {
-            if(homeController.isBannerLoading.value) {
-              return CarouselLoading();
+            if (homeController.bannerList.isNotEmpty) {
+              return CarouselSliderView(bannerList: homeController.bannerList);
             } else {
               return CarouselLoading();
             }
