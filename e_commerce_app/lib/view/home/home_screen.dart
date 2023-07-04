61b600ef1615b84e548503e5ffcd6260a29cd7a7
KengniJohan <107089322+KengniJohan@users.noreply.github.com>
2023-07-04 11:01:38 +0100

    utilisation de CarouselLoading

--- e_commerce_app/lib/view/home/home_screen.dart
+++ e_commerce_app/lib/view/home/home_screen.dart
@@ -1,5 +1,9 @@
 import 'package:e_commerce_app/component/main_header.dart';
+import 'package:e_commerce_app/controller/controllers.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+import 'components/carousel_slider/carousel_loading.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
@@ -9,7 +13,14 @@ class HomeScreen extends StatelessWidget {
     return SafeArea(
       child: Column(
         children: [
-          MainHeader()
+          const MainHeader(),
+          Obx(() {
+            if(homeController.isBannerLoading.value) {
+              return CarouselLoading();
+            } else {
+              return CarouselLoading();
+            }
+          })
         ],
       ),
     );
