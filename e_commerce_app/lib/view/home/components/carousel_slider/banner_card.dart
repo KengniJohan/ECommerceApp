93bdd8f6960984c15b9edf5839e6d7ca26351447
KengniJohan <107089322+KengniJohan@users.noreply.github.com>
2023-07-04 13:34:00 +0100

    Réalisation de la BannerCard

---
+++ e_commerce_app/lib/view/home/components/carousel_slider/banner_card.dart
@@ -0,0 +1,44 @@
+import 'package:e_commerce_app/const.dart';
+import 'package:flutter/material.dart';
+import 'package:cached_network_image/cached_network_image.dart';
+import 'package:shimmer/shimmer.dart';
+
+class BannerCard extends StatelessWidget {
+  final String imageUrl;
+  const BannerCard({super.key, required this.imageUrl});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.all(10),
+      child: ClipRRect(
+        borderRadius: const BorderRadius.all(Radius.circular(10)),
+        child: CachedNetworkImage(
+          imageUrl: "$baseUrl$imageUrl",
+          progressIndicatorBuilder: (context, url, progress) => Shimmer.fromColors(
+            highlightColor: Colors.white,
+            baseColor: Colors.grey.shade300,
+            child: Container(
+              margin: const EdgeInsets.all(10),
+              decoration: const BoxDecoration(
+                color: Colors.grey,
+                borderRadius: BorderRadius.all(Radius.circular(10)),
+              ),
+              child: ClipRRect(
+                borderRadius: const BorderRadius.all(Radius.circular(10)),
+                child: AspectRatio(
+                  aspectRatio: 16 / 9,
+                  child: Container(
+                    color: Colors.grey,
+                  ),
+                ),
+              ),
+            ),
+          ),
+          fit: BoxFit.cover,
+          width: double.infinity,
+        ),
+      ),
+    );
+  }
+}
