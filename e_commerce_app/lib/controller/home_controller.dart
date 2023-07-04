67bd77f047291c7d7205875b24a367d4ceeb53b1
KengniJohan <107089322+KengniJohan@users.noreply.github.com>
2023-07-04 15:49:53 +0100

    Récupération de la liste des PopularCategory

--- e_commerce_app/lib/controller/home_controller.dart
+++ e_commerce_app/lib/controller/home_controller.dart
@@ -1,3 +1,5 @@
+import 'package:e_commerce_app/model/category.dart';
+import 'package:e_commerce_app/service/remote_service/remote_popular_category_service.dart';
 import 'package:get/get.dart';
 
 import '../model/ad_banner.dart';
@@ -6,11 +8,16 @@ import '../service/remote_service/remote_banner_service.dart';
 class HomeController extends GetxController {
   static HomeController instance = Get.find();
   RxList<AdBanner> bannerList = List<AdBanner>.empty(growable: true).obs;
+
+  RxList<Category> popularCategoryList =
+      List<Category>.empty(growable: true).obs;
   RxBool isBannerLoading = false.obs;
+  RxBool isPopularCategoryLoading = false.obs;
 
   @override
   void onInit() {
     getAdBanners();
+    getPopularCategories();
     super.onInit();
   }
 
@@ -23,6 +30,18 @@ class HomeController extends GetxController {
       }
     } finally {
       isBannerLoading(false);
+    }
+  }
+
+  void getPopularCategories() async {
+    try {
+      isPopularCategoryLoading(true);
+      var result = await RemotePopularCategoryService().get();
+      if (result != null) {
+        popularCategoryList.assignAll(popularCategoryListFromJson(result.body));
+      }
+    } finally {
+      isPopularCategoryLoading(false);
     }
   }
 }
