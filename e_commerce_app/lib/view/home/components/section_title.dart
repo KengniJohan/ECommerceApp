1553adb3a4ad40b56af60eab708b3cb68e2b125d
KengniJohan <107089322+KengniJohan@users.noreply.github.com>
2023-07-05 07:55:32 +0100

    Réalisaton d'une section de titre pour les catégories populaires

---
+++ e_commerce_app/lib/view/home/components/section_title.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+class SectionTitle extends StatelessWidget {
+  final String title;
+  const SectionTitle({super.key, required this.title});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.fromLTRB(10, 10, 10, 5),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Text(
+            title,
+            style: const TextStyle(
+              fontSize: 18,
+              color: Colors.black,
+            ),
+          ),
+          const Text(
+            "See more",
+            style: TextStyle(
+              color: Colors.grey,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
