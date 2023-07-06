0c23aceda165039db6b45b4eed93a373abb5cd8d
KengniJohan <107089322+KengniJohan@users.noreply.github.com>
2023-07-06 16:45:28 +0100

    Réalisation du composant de abritant des cartes de chargement de produit populaire

---
+++ e_commerce_app/lib/view/home/components/popular_product/popular_product_loading.dart
@@ -0,0 +1,21 @@
+import 'package:e_commerce_app/view/home/components/popular_product/popular_product_loading_card.dart';
+import 'package:flutter/material.dart';
+
+class PopularProductLoading extends StatelessWidget {
+  const PopularProductLoading({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 220,
+      padding: const EdgeInsets.only(
+        right: 10,
+      ),
+      child: ListView.builder(
+          scrollDirection: Axis.horizontal,
+          physics: const BouncingScrollPhysics(),
+          itemCount: 5,
+          itemBuilder: (context, index) => const PopularProductLoadingCard()),
+    );
+  }
+}
