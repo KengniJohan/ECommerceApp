4fc3347e08adc46c022dc23ddcf9c5f8a1434e71
KengniJohan <107089322+KengniJohan@users.noreply.github.com>
2023-07-06 16:46:28 +0100

    Ajout d'une particularité scrollable et consommation de PopularProductLoading

--- e_commerce_app/lib/view/home/home_screen.dart
+++ e_commerce_app/lib/view/home/home_screen.dart
@@ -3,12 +3,12 @@ import 'package:e_commerce_app/controller/controllers.dart';
 import 'package:e_commerce_app/view/home/components/carousel_slider/carousel_slider_view.dart';
 import 'package:e_commerce_app/view/home/components/popular_category/popular_category.dart';
 import 'package:e_commerce_app/view/home/components/popular_category/popular_category_loading_card.dart';
+import 'package:e_commerce_app/view/home/components/popular_product/popular_product_loading.dart';
 import 'package:e_commerce_app/view/home/components/section_title.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
 import 'components/carousel_slider/carousel_loading.dart';
-import 'components/popular_category/popular_category_loading.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
@@ -19,23 +19,41 @@ class HomeScreen extends StatelessWidget {
       child: Column(
         children: [
           const MainHeader(),
-          Obx(() {
-            if (homeController.bannerList.isNotEmpty) {
-              return CarouselSliderView(bannerList: homeController.bannerList);
-            } else {
-              return const CarouselLoading();
-            }
-          }),
-          const SectionTitle(title: "Popular Category"),
-          Obx(() {
-            if (homeController.popularCategoryList.isNotEmpty) {
-              return PopularCategory(
-                categories: homeController.popularCategoryList,
-              );
-            } else {
-              return const PopularCategoryLoadingCard();
-            }
-          }),
+          Expanded(
+            child: SingleChildScrollView(
+              physics: const BouncingScrollPhysics(),
+              child: Column(
+                children: [
+                  Obx(() {
+                    if (homeController.bannerList.isNotEmpty) {
+                      return CarouselSliderView(
+                          bannerList: homeController.bannerList);
+                    } else {
+                      return const CarouselLoading();
+                    }
+                  }),
+                  const SectionTitle(title: "Popular Category"),
+                  Obx(() {
+                    if (homeController.popularCategoryList.isNotEmpty) {
+                      return PopularCategory(
+                        categories: homeController.popularCategoryList,
+                      );
+                    } else {
+                      return const PopularCategoryLoadingCard();
+                    }
+                  }),
+                  const SectionTitle(title: "Popular Product"),
+                  Obx(() {
+                    if (homeController.popularProductList.isNotEmpty) {
+                      return const PopularProductLoading();
+                    } else {
+                      return const PopularProductLoading();
+                    }
+                  }),
+                ],
+              ),
+            ),
+          ),
         ],
       ),
     );
