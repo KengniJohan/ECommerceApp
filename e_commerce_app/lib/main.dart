f730e441dd5f9dd09c556cf41e4d2a7ef661cf26
KengniJohan <107089322+KengniJohan@users.noreply.github.com>
2023-07-07 11:06:01 +0100

    Enregistrement des adapters des Adapteurs générés dans Hive

--- e_commerce_app/lib/main.dart
+++ e_commerce_app/lib/main.dart
@@ -1,10 +1,21 @@
+import 'package:e_commerce_app/model/ad_banner.dart';
+import 'package:e_commerce_app/model/category.dart';
+import 'package:e_commerce_app/model/product.dart';
 import 'package:e_commerce_app/route/app_page.dart';
 import 'package:e_commerce_app/route/app_route.dart';
 import 'package:e_commerce_app/theme/app_theme.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:hive_flutter/hive_flutter.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Hive.initFlutter();
+
+  // Register adapters
+  Hive.registerAdapter(AdBannerAdapter());
+  Hive.registerAdapter(CategoryAdapter());
+  Hive.registerAdapter(ProductAdapter());
   runApp(const MyApp());
 }
 
